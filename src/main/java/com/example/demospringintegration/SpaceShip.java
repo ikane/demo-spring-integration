b9ab8d39e14c1e5bdc30d7f5a1f8e7f6086ff900
ikane <kaneelhadji2610@yahoo.fr>
2022-08-13 11:41:47 +0200

    first commit

---
+++ src/main/java/com/example/demospringintegration/SpaceShip.java
@@ -0,0 +1,9 @@
+package com.example.demospringintegration;
+
+import lombok.Data;
+
+@Data
+public class SpaceShip {
+	String name;
+	double fuel = 15.0d;
+}
---
+++ src/test/java/com/example/demospringintegration/DemoSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
